6f5fdd909f6b0fa6090ffb5fd1d03b47ccd39390
davidlijar <davidbeljar@gmail.com>
2023-06-13 16:44:09 +0900

    ad

--- test_15_05.cpp
+++ test_15_05.cpp
@@ -5,13 +5,11 @@ int main() {
 	
 	int data[100];
 	
-	while(fscanf(fp,"%d", data)!=EOF){
-		printf("%d", data);
-	}
+	fscanf(fp,"%d %d", &data[0], &data[1]);
 	
 	printf("%d", data[0]);
 	
-	for(int i=data[0]; i<data[1]; i++){
+	for(int i=data[0]; i<=data[1]; i++){
 		for(int j=1; j<10; j++){
 			printf("%d x %d = %d\n", i,j,i*j);
 		}printf("\n");
